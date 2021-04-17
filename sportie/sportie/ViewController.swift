a86bae7c7057abfd3ad071b855291afc2d058f52
Yasmine Ghazy <yasmine.ghazyfciscu@gmail.com>
2021-04-17 23:27:59 +0200

    setup-project #comment Create XCode projct and init pod file to create XCworkspace #done

---
+++ sportie/sportie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sportie
+//
+//  Created by Yasmine Ghazy on 17/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
